d43cb65ee1a83fb7a3ee734916824b23f99f2b89
Particle <particlecore@outlook.com>
2018-02-05 17:52:33 -0100

    0.0.5
    Closes #324

--- src/Userscript/Iridium.user.js
+++ src/Userscript/Iridium.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @version         0.0.4
+// @version         0.0.5
 // @name            Iridium
 // @namespace       https://github.com/ParticleCore
 // @description     YouTube with more freedom
--- src/Webextension/js/Iridium.user.js
+++ src/Webextension/js/Iridium.user.js
@@ -1,5 +1,5 @@
 ﻿// ==UserScript==
-// @version         0.0.4
+// @version         0.0.5
 // @name            Iridium
 // @namespace       https://github.com/ParticleCore
 // @description     YouTube with more freedom

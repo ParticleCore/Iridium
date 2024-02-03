f8a91c50fb8663d9c7d7f1b97d10df85ebda2f2d
Particle <particlecore@outlook.com>
2024-02-03 17:38:11 -0500

    Fix unsafe innerHTML usage

--- src/js/options.js
+++ src/js/options.js
@@ -467,10 +467,12 @@ const Manager = {
                 child.dataset.handle = newList[item].handle;
                 child.dataset.ucid = item;
                 child.innerHTML = `
-                    <div class="channelName">${newList[item].name || "<i>@" + newList[item].handle + "</i>"}</div>
+                    <div class="channelName"></div>
                     <svg class="remove" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
                         <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/>
                     </svg>`;
+
+                child.querySelector(".channelName").textContent = newList[item].name || "<i>@" + newList[item].handle + "</i>";
 
                 newNodeList.push(child);
 
